a48037162b26525f19b76d769058b29b5cb7c405
Amanda McGivern <amanda@flexpa.com>
2023-06-24 13:10:56 -0400

    Sort projects using reward function

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -13,6 +13,7 @@ import { trpc } from "../trpc";
 // import { createPublicClient, http } from 'viem'; 
 import { createPublicClient, http } from 'viem'
 import { AppContextProvider, useAppContext } from "@/context";
+import { MetricType } from "@prisma/client";
 
 // const publicClient = createPublicClient({
 //   chain: mainnet,
@@ -43,7 +44,6 @@ const wagmiConfig = createConfig({
 
 
 export default function App() {
-  const { setProjects } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
   const [state, setState] = useState<any | null>(null);
 
@@ -53,10 +53,10 @@ export default function App() {
       .then((data) => {
         setState(data);
         console.log(data);
-        setProjects(data.projects)
+
         setIsLoading(false);
       });
-  }, [setProjects]);
+  }, []);
 
   if (isLoading) {
     return (
@@ -73,7 +73,67 @@ export default function App() {
 function Main({ state }: { state: GlobalState }) {
   const { setProjects } = useAppContext();
   useEffect(() => {
-    setProjects(state.projects);
+        setProjects(state.projects);
+        // @TODO - remove hardcoded projects
+      //   setProjects([
+      //     {
+      //         "id": "f4f67524-1728-4aed-94a1-85590d003464",
+      //         "githubLink": "https://github.com/wagmi-dev/wagmi",
+      //         "npmLink": "https://www.npmjs.com/package/wagmi",
+      //         "isTest": true,
+      //         "metrics": [
+      //           { id: 1,
+      //             metricType: MetricType.NUM_DEPENDANTS,
+      //             value: 2,
+      //             projectId: "f4f67524-1728-4aed-94a1-85590d003464"
+      //           },
+      //           { id: 2,
+      //             metricType: MetricType.NUM_GITHUB_CONTRIBUTORS,
+      //             value: 4,
+      //             projectId: "f4f67524-1728-4aed-94a1-85590d003464"
+      //           },
+      //           { id: 3,
+      //             metricType: MetricType.NUM_GITHUB_STARS,
+      //             value: 6,
+      //             projectId: "f4f67524-1728-4aed-94a1-85590d003464"
+      //           },
+      //           { id: 4,
+      //             metricType: MetricType.NUM_NPM_DOWNLOADS,
+      //             value: 8,
+      //             projectId: "f4f67524-1728-4aed-94a1-85590d003464"
+      //           },
+      //         ]
+      //     },
+      //     {
+      //       "id": "f4f67524-1728-4aed-94a1-85590d003463",
+      //       "githubLink": "https://github.com/wagmi-dev/nope",
+      //       "npmLink": "https://www.npmjs.com/package/123",
+      //       "isTest": true,
+      //       "metrics": [
+      //         { id: 1,
+      //           metricType: MetricType.NUM_DEPENDANTS,
+      //           value: 8,
+      //           projectId: "f4f67524-1728-4aed-94a1-85590d003463"
+      //         },
+      //         { id: 2,
+      //           metricType: MetricType.NUM_GITHUB_CONTRIBUTORS,
+      //           value: 6,
+      //           projectId: "f4f67524-1728-4aed-94a1-85590d003463"
+      //         },
+      //         { id: 3,
+      //           metricType: MetricType.NUM_GITHUB_STARS,
+      //           value: 4,
+      //           projectId: "f4f67524-1728-4aed-94a1-85590d003463"
+      //         },
+      //         { id: 4,
+      //           metricType: MetricType.NUM_NPM_DOWNLOADS,
+      //           value: 2,
+      //           projectId: "f4f67524-1728-4aed-94a1-85590d003463"
+      //         },
+      //       ]
+      //   }
+      // ]
+  // )
   }, [state.projects, setProjects]);
 
   return <div>
--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -14,22 +14,21 @@ function ProjectList () {
     const result: number =  project.metrics.reduce((acc, metric) => {
       return acc + calculateMetricValue(metric, weights);
     }, 0);
-    console.log("result", result);
     return result;  
   }
   
   return (
     <div>
-      {projects.sort((a,b) => rewardFunction(a, weights) - rewardFunction(b, weights)).map(project => <ProjectCard key={project.id} project={project} />)}
+      {projects.sort((a,b) => rewardFunction(a, weights) - rewardFunction(b, weights)).map((project, index) => <ProjectCard key={project.id} project={project} index={index + 1} />)}
     </div>
   )
 }
 
-function ProjectCard ({project}: {project: ProjectWithMetrics}) {
+function ProjectCard ({project, index}: {project: ProjectWithMetrics, index: number}) {
   return (
   <div className="p-2 bg-slate-100 border-2 border-slate-300 flex gap-2 flex-wrap">
     <div>
-      Rank 1
+      {index}
     </div>
     <div>
       <div className="w-full">{project.id}</div>
