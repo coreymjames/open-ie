a02b1f933c177102cbed8b652e1f9380a5561a49
Amanda McGivern <amanda@flexpa.com>
2023-06-24 16:50:26 -0400

    Style projects

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -1,6 +1,7 @@
 import { useAppContext } from "@/context";
 import { ProjectWithMetrics, Weight } from "@/types";
 import { Metric } from "@prisma/client";
+import { useState } from "react";
 
 function ProjectList() {
   const { projects, weights } = useAppContext();
@@ -20,8 +21,8 @@ function ProjectList() {
   }
   //
   return (
-    <div className="m-4 divide-y border-gray-300 divide-gray-300  border-x-[1px] last:border-b-[1px] first:border-t-[1px] first:rounded-t">
-      <div className="rounded-t p-4 bg-gray-100"></div>
+    <div className="m-4 divide-y divide-gray-300 border-x-[1px]  border-gray-300 first:rounded-t first:border-t-[1px] last:border-b-[1px]">
+      <div className="rounded-t bg-gray-100 p-4"></div>
       {projects
         .sort((a, b) => rewardFunction(a, weights) - rewardFunction(b, weights))
         .map((project, index) => (
@@ -38,34 +39,63 @@ function ProjectCard({
   project: ProjectWithMetrics;
   index: number;
 }) {
+  const [showDetails, setShowDetails] = useState(false);
   return (
-    <div className="w-full p-2 odd:bg-gray-50 even:bg-white flex gap-2 flex-wrap">
-      <div>{index}</div>
-      <div className="w-full">
-        <div className="w-full">Project Name</div>
-        <div className="flex justify-evenly w-full">
+    <div className="flex w-full flex-wrap gap-2 p-4 px-6 odd:bg-gray-50 even:bg-white">
+      <div className="flex w-full items-center p-2">
+        {/* <div className="px-1">{index}.</div> */}
+        <h4 className="flex-1 whitespace-nowrap text-xl font-light">
+          Project Name
+        </h4>
+        <div className="flex w-full justify-evenly ">
           {project.metrics.map((metric) => (
             <ProjectMetric key={metric.id} metric={metric} />
           ))}
         </div>
-        <div>{project.githubLink}</div>
-        <div>{project.npmLink}</div>
+        <div
+          className="cursor-pointer"
+          onClick={() => setShowDetails((show: boolean) => !show)}
+        >
+          v
+        </div>
       </div>
+      {showDetails && (
+        <div className="grid w-full grid-cols-4 border-t-[1px] border-gray-300 px-2 pt-4 text-sm">
+          <div className="col-span-3 mr-12">
+            <h5 className="mb-1 font-semibold">Project Summary</h5>
+            <p className="font-light">
+              TODO Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flex flex-col border-l-[1px] pl-4">
+            <h5 className="mb-1 font-semibold">Project Links</h5>
+            <a className="font-light underline" href={project.githubLink}>
+              Github
+            </a>
+            <a className="font-light underline" href={project.npmLink}>
+              NPM
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 const MetricTypeDisplay = {
   NUM_DEPENDANTS: "Dependents",
-  NUM_GITHUB_CONTRIBUTORS: "Github Contributors",
-  NUM_GITHUB_STARS: "Github Stars",
-  NUM_NPM_DOWNLOADS: "NPM Downloads",
+  NUM_GITHUB_CONTRIBUTORS: "Contributors",
+  NUM_GITHUB_STARS: "Stars",
+  NUM_NPM_DOWNLOADS: "Downloads",
 };
 
 function ProjectMetric({ metric }: { metric: Metric }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="text-xl">{metric.value}</div>
+      <div className="text-xl font-medium">{metric.value}</div>
       <div className="text-xs">{MetricTypeDisplay[metric.metricType]}</div>
     </div>
   );
